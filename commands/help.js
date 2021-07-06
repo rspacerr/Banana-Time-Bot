fc382ea29936e79d43f25444607d5139b231c654
rspacerr <definitelynotoberon@gmail.com>
2021-07-06 19:16:42 -0400

    Formatted help DM

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,20 @@
 module.exports = {
-    name: 'help',
-    description: "List of commands.",
+    name: `help`,
+    description: `The command you just used`,
     execute(message, args) {
         const data = []
         const { commands } = message.client
 
+        /* If no argument specified */
         if (!args.length) {
             console.log("Running: .help")
-            data.push('Here\'s a list of all the commands:')
-            data.push(commands.map(command => command.name).join(', '))
+            data.push('Bot Commands:')
+            data.push(commands.map(command => `**` + command.name + `:** ` + command.description).join('\n'))
             data.push(`\nUse .help [command name] for help on a specific command`)
-            message.channel.send("```Test```")  
             return message.author.send(data, { split: true })
                 .then(() => {
                     if (message.channel.type === 'dm') return;
-                    message.channel.send("DM Sent!")
+                    message.channel.send(`DM Sent!`)
                 })
                 .catch(error => {
                     console.error(`Could not send help DM to ${message.author.tag}.\n`, error)
@@ -22,6 +22,7 @@ module.exports = {
                 })
         }
 
+        /* Give description of specified command */
         const name = args[0].toLowerCase()
         const command = commands.get(name) || commands.find(c => aliases && c.aliases.includes(name))
 
@@ -29,8 +30,8 @@ module.exports = {
             message.channel.send("That's not a valid command, try again")
             return
         }
-        data.push(`**${command.name}**`)
-        if (command.description) data.push(`${command.description}`)
+
+        if (command.description) data.push(`**${command.name}** - ${command.description}`)
         else message.channel.send(`Hmm...no description found. You should probably report that to r rr.`)
 
         message.channel.send(data, { split: true })
